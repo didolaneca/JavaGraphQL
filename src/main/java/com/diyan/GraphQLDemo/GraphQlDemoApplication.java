0fd179320e11f0a5e6e17be0f0966797455de575
Diyan Simeonov <diyan_simeonov@yahoo.com>
2022-06-14 17:26:24 -0500

    Added a runtimeWiringConfigurer needed to enable query of data

--- src/main/java/com/diyan/GraphQLDemo/GraphQlDemoApplication.java
+++ src/main/java/com/diyan/GraphQLDemo/GraphQlDemoApplication.java
@@ -1,7 +1,10 @@
 package com.diyan.GraphQLDemo;
 
+import com.diyan.GraphQLDemo.services.CrmService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.graphql.execution.RuntimeWiringConfigurer;
 
 @SpringBootApplication
 public class GraphQlDemoApplication {
@@ -10,4 +13,10 @@ public class GraphQlDemoApplication {
 		SpringApplication.run(GraphQlDemoApplication.class, args);
 	}
 
+	@Bean
+	RuntimeWiringConfigurer runtimeWiringConfigurer(CrmService crm){
+		return builder -> builder.type("Query", wiring -> wiring
+				.dataFetcher("customerById", env -> crm.getCustomerById(Integer.parseInt(env.getArgument("id"))))
+				.dataFetcher("customers", env -> crm.getCustomers()));
+	}
 }
